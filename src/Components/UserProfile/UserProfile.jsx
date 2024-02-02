a16006847bb4e6bb1dd57a8ce89947691431da15
xFinVi <111379135+xFinVi@users.noreply.github.com>
2024-02-02 22:59:30 +0000

    Update UserProfile.jsx

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,111 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import { setUser,setOrders } from '../../redux/userSlice';
-import axios from 'axios';
-import Logout from '../Logout/Logout'
-import './userprofile.css'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUser } from '../../redux/userSlice';
+import Logout from '../Logout/Logout';
+import './userprofile.css';
 import OrdersDetailsModal from '../Modal/OrdersDetailsModal';
-
 import { FaUser } from "react-icons/fa";
+
 const UserProfile = () => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-  const userId = useSelector((state) => state.user?.value?.user?.user_id);
-  const sessionId = useSelector((state) => state.user); // Retrieve session ID from state
-  const dispatch = useDispatch()
-  const [userData, setUserData] = useState(null);
-  const [userOrders, setUserOrders] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const userData = useSelector((state) => state.user.value);
+  const userOrders = useSelector((state) => state.user.orders);
+  const dispatch = useDispatch();
   const [modal, setModal] = useState(null);
 
-  useEffect(() => {
-   const fetchUserData = async () => {
-      try {
-        const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}users/${userId}`  ,   
-        {withCredentials: true});
-        
-        setUserData(response.data);
-        dispatch(setUser(response.data));
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-        setLoading(false);
-      }
-    }; 
-    const fetchUserOrders = async () => {
-      try {
-        const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}orders/${userId}`,{withCredentials:true});
-        setUserOrders(response.data);
-        dispatch(setOrders(response.data));
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching user orders:', error);
-        setLoading(false);
-      }
-    }; 
-    
+  const deleteUser = async () => {
+    // Implement delete user functionality
+  };
 
-    
-    if (isAuthenticated) {
-      fetchUserData();
-      fetchUserOrders();
-      console.log(sessionId)
-    }    
- 
-  }, [isAuthenticated]);
+  const openModal = (order) => {
+    console.log(userData)
+    setModal(order);
+  };
+
+  const closeModal = () => {
+    setModal(null);
+  };
 
   if (!isAuthenticated) {
     return <div>Please log in to view your profile.</div>;
   }
 
-   if (loading) {
-    return <div>Loading...</div>;
-   } 
-const deleteUser = async () => {
-  
-};
-const openModal = (order) => {
-  setModal(order);
-};
-
-const closeModal = () => {
-  setModal(null);
-};
-
-
   return (
     <div className="user-profile">
-    <div className="user-header">
-      <div className="user-info">
-        <h2>{userData && `${userData[0].first_name} ${userData[0].last_name}`}</h2>
-        <p>{userData && userData[0].email}</p>
+      <div className="user-header">
+        <div className="user-info">
+          <h2>{userData && `${userData.user.name}`}</h2>
+          <p>{userData && userData.user.email}</p>
+        </div>
+        <div className="user-avatar">
+          <FaUser className="user_details-icon" />
+        </div>
       </div>
-      <div className="user-avatar">
-      <FaUser className="user_details-icon"/>
+      <div className="user-details">
+        <h3>HELLO</h3>
+        {userData && (
+          <>
+            <p> {`${userData.user.name} `}</p>
+            <p>Email: {userData.user.email}</p>
+            <p>Role: <b> {userData.user.role}</b></p>
+          </>
+        )}
       </div>
-    </div>
-    <div className="user-details">
-      <h3>User Details</h3>
-      {userData && (
-        <>
-          <p>Name: {`${userData[0].first_name} ${userData[0].last_name}`}</p>
-          <p>Email: {userData[0].email}</p>
-        </>
-      )}
-    </div>
-    <div className="order-history">
-      <h3>Order History</h3>
-      <ul>
-        {userOrders.map((order) => (
-          <li key={order.order_id}>
-           <span onClick={() => openModal(order)}>Order ID: {order.order_id},</span>  <span>Total: {order.total_price}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-    <div className="user-utilities">
-      <div className='logout'>Logout:<Logout /></div>
-      <div className='del-user'> <p onClick={deleteUser}>Delete User</p></div>
-     
+      <div className="order-history">
+        <h3>Order History</h3>
+        <ul>
+          {userOrders.map((order) => (
+            <li key={order.order_id}>
+              <span onClick={() => openModal(order)}>Order ID: {order.order_id},</span>  <span>Total: {order.total_price}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="user-utilities">
+        <div className='logout'>Logout:<Logout /></div>
+        <div className='del-user'> <p onClick={deleteUser}>Delete User</p></div>
       </div>
       {modal && (
         <div className="overlay">
@@ -114,8 +72,8 @@ const closeModal = () => {
           </div>
         </div>
       )}
-  </div>
-);
+    </div>
+  );
 };
 
 export default UserProfile;
